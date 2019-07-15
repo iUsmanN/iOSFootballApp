fcf669473f139231993085a28c9ff4cff30fb985
usmannazirtintash <usman.nazir@tintash.com>
2019-07-15 11:53:21 +0500

    Added Project to Git

---
+++ iOSTrainingProject/iOSTrainingProject/iOSTrainingProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSTrainingProject
+//
+//  Created by Usman Nazir on 15/07/2019.
+//  Copyright © 2019 Usman Nazir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
