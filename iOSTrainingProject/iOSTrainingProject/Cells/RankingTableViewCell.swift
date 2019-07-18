6c5f8123bb5dcd035f85903fffa2ad8c70f06616
usmannazirtintash <usman.nazir@tintash.com>
2019-07-18 13:11:06 +0500

    Completed teams tableview

--- iOSTrainingProject/iOSTrainingProject/Cells/RankingTableViewCell.swift
+++ iOSTrainingProject/iOSTrainingProject/Cells/RankingTableViewCell.swift
@@ -8,8 +8,8 @@
 
 import UIKit
 
-class RankingTableViewCell: UITableViewCell {
-
+class RankingTableViewCell: UITableViewCell, Color {
+    
     var item: RankingItem?
     
     @IBOutlet weak var ranking: UILabel!
@@ -21,10 +21,10 @@ class RankingTableViewCell: UITableViewCell {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
         if let r = item?.ranking
         {
@@ -36,6 +36,34 @@ class RankingTableViewCell: UITableViewCell {
             teamName.text = n
         }
         
+        
+        DispatchQueue.global(qos: .background).async {
+            if let f = URL(string: self.item?.flag ?? "")
+            {
+                do{
+                    let data = try Data(contentsOf: f)
+                    DispatchQueue.main.async { self.flag.image = UIImage(data: data) }
+                } catch {
+                    print("Error in loading data")
+                }
+            }
+        }
+        
+        if let p = item?.position
+        {
+            if(p>0)
+            {
+                position.image = UIImage(named: "up")?.withRenderingMode(.alwaysTemplate)
+                position.tintColor = getColor(0,0.7,0,1)
+            } else if (p<0)
+            {
+                position.image = UIImage(named: "down")?.withRenderingMode(.alwaysTemplate)
+                position.tintColor = getColor(0.7,0,0,1)
+            } else {
+                position.image = UIImage(named: "neutral")?.withRenderingMode(.alwaysTemplate)
+                position.tintColor = getColor(0,0,0,1)
+            }
+        }
     }
-
+    
 }
---
+++ iOSTrainingProject/iOSTrainingProject/Colors/Colors.swift
@@ -0,0 +1,23 @@
+//
+//  Colors.swift
+//  iOSTrainingProject
+//
+//  Created by Usman Nazir on 18/07/2019.
+//  Copyright © 2019 Usman Nazir. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+protocol Color
+{
+    func getColor(_ r: CGFloat, _ g: CGFloat, _ b: CGFloat, _ alpha: CGFloat) -> UIColor
+}
+
+extension Color
+{
+    func getColor(_ r: CGFloat, _ g: CGFloat, _ b: CGFloat, _ alpha: CGFloat) -> UIColor
+    {
+        return UIColor(red: r, green: g, blue: b, alpha: alpha)
+    }
+}
